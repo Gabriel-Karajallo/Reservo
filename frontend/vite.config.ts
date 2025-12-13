a9d108f34e8c17bbdaffa6f908bfab8067020ac9
Gabriel-Karajallo <c.gabrielkarajallo@gmail.com>
2025-12-13 01:54:58 +0100

    IconosPWA: Se agrega iconos para PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,21 +20,38 @@ export default defineConfig({
         short_name: 'Reservo',
         description: 'Reserva tus servicios favoritos al instante.',
         theme_color: '#0f6f63',
-        background_color: '#ffffff',
+        background_color: "#0f6f63",
         display: 'standalone',
         start_url: '/cliente/home',
         icons: [
-          {
-            src: 'icons/icono192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icono512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ]
+    // iconos de instalación
+    {
+      src: "icons/icon-192.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "any"
+    },
+    {
+      src: "icons/icon-512.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "any"
+    },
+
+    // splash screen para android
+    {
+      src: "icons/titulo192.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "maskable"
+    },
+    {
+      src: "icons/titulo512.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "maskable"
+    }
+  ]
       }
     })
   ],
