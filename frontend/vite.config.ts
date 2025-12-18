91dbd772973c79c26c70af5139557cf06100acb8
Gabriel-Karajallo <c.gabrielkarajallo@gmail.com>
2025-12-18 10:56:06 +0100

    despliegue y cambio de iconos PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,42 +8,44 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
-  registerType: 'autoUpdate',
-  manifest: {
-    name: 'Reservo',
-    short_name: 'Reservo',
-    description: 'Reserva tus servicios favoritos al instante.',
-    display: 'standalone',
-    start_url: '/',
-    scope: '/',
-    theme_color: '#1F2A44',
-    background_color: '#1F2A44',
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Reservo',
+        short_name: 'Reservo',
+        description: 'Reserva tus servicios favoritos al instante.',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        theme_color: '#1F2A44',
+        background_color: '#1F2A44',
 
-    icons: [
-      // ICONO DE LA APP (la R)
-       {
-    "src": "/icons/icon-192.png",
-    "sizes": "192x192",
-    "type": "image/png"
-  },
-  {
-    "src": "/icons/icon-512.png",
-    "sizes": "512x512",
-    "type": "image/png"
-  },
+        icons: [
+          // ICONO APP → SOLO LA R
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
 
-      // SPLASH SCREEN (logo Reservo texto)
-      {
-        "src": "../public/titulo.png",
-        "sizes": "512x512",
-        "type": "image/png"
+          // ICONO USADO PARA SPLASH → TEXTO "RESERVO"
+          {
+            src: '/icons/splash-icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
-    ]
-  }
-})
+    })
   ],
 
   server: {
-    origin: "http://localhost:5173",
+    origin: 'http://localhost:5173'
   }
 });
